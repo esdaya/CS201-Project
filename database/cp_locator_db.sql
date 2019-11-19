eb1d3a6e0e9023af9668241c51d13f7b5761236a
Dhivya Senthil <senthilm@usc.edu>
2019-11-19 11:54:27 -0800

    Create cp_locator_db.sql
    basic db creation sql statements

---
+++ database/cp_locator_db.sql
@@ -0,0 +1,29 @@
+--creates CP Locator database and tables
+
+CREATE DATABASE cp_locator;
+
+CREATE TABLE classes (
+  class_id int NOT NULL,
+  class_name varchar(255) NOT NULL,
+  class_code varchar(255) NOT NULL,  --this is the class passphrase (validation)
+  PRIMARY KEY (class_id)
+);
+
+CREATE TABLE cps (
+  cp_id int NOT NULL,
+  cp_name varchar(255) NOT NULL,  --will return this for interfacing
+  cp_email varchar(255) NOT NULL,  --login w/ email and password?
+  cp_password varchar(255) NOT NULL,
+  PRIMARY KEY (class_id)
+);
+
+CREATE TABLE current_cps (
+  FOREIGN KEY (cp_id) REFERENCES cps(cp_id),
+  FOREIGN KEY (class_id) REFERENCES classes(class_id)
+);
+
+CREATE TABLE student_queue (
+  student_name varchar(255) NOT NULL,
+  student_email varchar(255),  --allowed to submit w/o email
+  FOREIGN KEY (class_id) REFERENCES classes(class_id)
+);
