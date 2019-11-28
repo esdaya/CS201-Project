9ab96d98282d6acf91538dee4e624e7dac1dca9c
Dhivya Senthil <senthilm@usc.edu>
2019-11-27 20:10:55 -0800

    Update cp_locator_db.sql
    Added AI/fixed errors

--- database/cp_locator_db.sql
+++ database/cp_locator_db.sql
@@ -3,18 +3,18 @@
 CREATE DATABASE cp_locator;
 
 CREATE TABLE classes (
-  class_id int NOT NULL,
+  class_id int NOT NULL AUTO_INCREMENT,
   class_name varchar(255) NOT NULL,
   class_code varchar(255) NOT NULL,  --this is the class passphrase (validation)
   PRIMARY KEY (class_id)
 );
 
 CREATE TABLE cps (
-  cp_id int NOT NULL,
+  cp_id int NOT NULL AUTO_INCREMENT,
   cp_name varchar(255) NOT NULL,  --will return this for interfacing
   cp_email varchar(255) NOT NULL,  --login w/ email and password?
   cp_password varchar(255) NOT NULL,
-  PRIMARY KEY (class_id)
+  PRIMARY KEY (cp_id)
 );
 
 CREATE TABLE current_cps (
@@ -23,7 +23,9 @@ CREATE TABLE current_cps (
 );
 
 CREATE TABLE student_queue (
+  student_id int NOT NULL AUTO_INCREMENT,
   student_name varchar(255) NOT NULL,
   student_email varchar(255),  --allowed to submit w/o email
   FOREIGN KEY (class_id) REFERENCES classes(class_id)
+  PRIMARY KEY (student_id)
 );
