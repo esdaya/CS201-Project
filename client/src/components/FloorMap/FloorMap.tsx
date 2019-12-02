3d2e93129de23f0282caa4cc0cd5c04335a83d53
Matt Evenson <mevenson@usc.edu>
2019-12-02 14:55:00 -0800

    Add mappings from tables to offset percentages

--- client/src/components/FloorMap/FloorMap.tsx
+++ client/src/components/FloorMap/FloorMap.tsx
@@ -1,5 +1,27 @@
 import React from "react";
 
+const locations = [
+  { left: "10%", top: "48%" },
+  { left: "28%", top: "48%" },
+  { left: "48%", top: "55%" },
+  { left: "63%", top: "55%" },
+  { left: "77%", top: "55%" },
+  { left: "77%", top: "64%" },
+  { left: "77%", top: "72%" },
+  { left: "77%", top: "81%" },
+  { left: "72%", top: "90%" },
+  { left: "61%", top: "88%" },
+  { left: "51%", top: "88%" },
+  { left: "47%", top: "80%" },
+  { left: "28%", top: "70%" },
+  { left: "82%", top: "18%" },
+  { left: "94%", top: "8%" },
+  { left: "90%", top: "43%" },
+  { left: "90%", top: "31%" },
+  { left: "64%", top: "4%" },
+  { left: "46%", top: "4%" }
+];
+
 const FloorMap: React.FC = () => {
   return <div>Hello world!</div>;
 };
